58857f74539b431e88763bd8b34f4d19f9301e59
Jayson Hibbard <jhibbard055@gmail.com>
2021-07-06 10:48:36 -0600

    Add Conditionals to Task Submit Form

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,25 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../StyleSheets/Project.css";
 import add from "./Images/add.svg";
+import xIcon from "./Images/x-symbol.svg";
+import firebase from "../firebase";
 
 function Project() {
   const [newProject, setNewProject] = useState("");
   const [newTask, setNewTask] = useState("");
   const [projectList, setProjectList] = useState([]);
   const [taskList, setTaskList] = useState([]);
+  const [currentProject, setCurrentProject] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // function to get project list from database
+  const fetchData = () => {
+    // es6 arrow function
+    const ref = firebase.database().ref("projects"); // get reference to part of db we want
+    ref.on("value", (snapshot) => {
+      // fire this code block on any value change in this section of db
+      let data = snapshot.val(); // save value of object that is tied to the reference point we specified earlier
+      let newList = []; // empty list to append data to later
+      for (let d in data) {
+        // for-loop to append each project to the array from the line above
+        newList.push(data[d].project); // .push adds each item that for-loop passes over into array
+      }
+      // console.log(newList);
+      setProjectList(newList);
+    });
+  };
+
+  // function to update current project useState variable
+  const updateCurrentProject = (e) => {
+    // runs on onClick for any project listed on the screen
+    const projectName = e.target;
+    const projs = document.getElementsByTagName("p");
+    for (let i = 0; i < projs.length; i++) {
+      projs[i].className = "";
+    }
+    projectName.className = "project__list-selected-item";
+    // set currentProject useState var
+    const proj = e.target.innerText;
+    setCurrentProject(proj);
+    // console.log(currentProject);
+  };
 
   const handleProjectSubmit = (e) => {
     e.preventDefault();
     setProjectList([...projectList, newProject]);
+    const projectsRef = firebase.database().ref("projects/");
+    const project = {
+      project: newProject,
+      tasks: taskList,
+    };
+    projectsRef.push(project);
     // alert(`Added Project: ${newProject}`);
     setNewProject("");
   };
 
   const handleTaskSubmit = (e) => {
     e.preventDefault();
-    setTaskList([...taskList, newTask]);
-    // alert(`${newTask} task added to project(x)`);
-    setNewTask("");
+    let projectRef = firebase
+      .database()
+      .ref("projects/")
+      .orderByChild("project")
+      .equalTo(currentProject);
+    if (newTask !== "") {
+      if (currentProject !== "") {
+        setTaskList([...taskList, newTask]);
+        setNewTask("");
+      } else {
+        alert("Please select a project before adding a task :)");
+      }
+    } else {
+      alert("Please fill in the field before submitting :)");
+    }
   };
 
   const handleMouseEnter = (e) => {
@@ -34,6 +91,23 @@ function Project() {
     }
   };
 
+  const handleRemoveTask = (index) => {
+    let newTaskList = [...taskList];
+    newTaskList.splice(index, 1);
+    setTaskList(newTaskList);
+  };
+
+  const handleRemoveProject = (index) => {
+    let clickedProject = projectList[index];
+    // removing from the UI
+    let newProjectList = [...projectList];
+    newProjectList.splice(index, 1);
+    setProjectList(newProjectList);
+    // removing from the DB
+    let projectRef = firebase.database().ref("projects/");
+    projectRef.remove();
+  };
+
   return (
     <div className="project">
       <div className="project__div">
@@ -41,8 +115,17 @@ function Project() {
         <div className="project__div--line"></div>
         {/* insert items into list from database */}
         <ul className="project__list">
-          {projectList.map((item) => {
-            return <li>{item}</li>;
+          {projectList.map((item, index) => {
+            return (
+              <li key={index}>
+                <p onClick={(e) => updateCurrentProject(e)}>{item}</p>
+                <img
+                  src={xIcon}
+                  className="x-icon"
+                  onClick={() => handleRemoveProject(index)}
+                />
+              </li>
+            );
           })}
         </ul>
         <input
@@ -53,14 +136,23 @@ function Project() {
           onMouseLeave={handleMouseLeave}
           onChange={(e) => setNewProject(e.target.value)}
         />
-        <img src={add} onClick={handleProjectSubmit} />
+        <img src={add} onClick={handleProjectSubmit} className="add-icon" />
       </div>
       <div className="project__div">
         <h2 className="project__title">Tasks</h2>
         <div className="project__div--line"></div>
         <ul className="project__list">
           {taskList.map((item) => {
-            return <li>{item}</li>;
+            return (
+              <li key={item}>
+                {item}
+                <img
+                  src={xIcon}
+                  className="x-icon"
+                  onClick={() => handleRemoveTask}
+                />
+              </li>
+            );
           })}
         </ul>
         <input
@@ -71,7 +163,7 @@ function Project() {
           onMouseLeave={handleMouseLeave}
           onChange={(e) => setNewTask(e.target.value)}
         />
-        <img src={add} onClick={handleTaskSubmit} />
+        <img src={add} onClick={handleTaskSubmit} className="add-icon" />
       </div>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCDY4woviBTGy-IMONA5Xea_z5zEdSEuO0",
+  authDomain: "code-manager-47d20.firebaseapp.com",
+  projectId: "code-manager-47d20",
+  storageBucket: "code-manager-47d20.appspot.com",
+  messagingSenderId: "245646087993",
+  appId: "1:245646087993:web:16097de1bc60c016833ae8",
+  measurementId: "G-51DGX958W6",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
